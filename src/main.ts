4f21d3a65e3cb76b3bd342ea34eb7494594a74f7
snahmd <sanahmed7@gmail.com>
2024-09-09 18:19:38 +0200

    Add product fetching and display functionality

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,48 @@
+import IProduct from "./interfaces/IProduct";
+const titleInput = document.getElementById("itemTitle") as HTMLInputElement;
+const sortByButton = document.getElementById("sortBy") as HTMLButtonElement;
+const filterElectronicsButton = document.getElementById(
+  "electronics"
+) as HTMLButtonElement;
+const filterJewelerysButton = document.getElementById(
+  "jewelery"
+) as HTMLButtonElement;
+const filterMensButton = document.getElementById("mens") as HTMLButtonElement;
+const filterWomensButton = document.getElementById(
+  "womens"
+) as HTMLButtonElement;
+const productsContainer = document.getElementById(
+  "itemsSection"
+) as HTMLDivElement;
 
+fetchAndDisplay();
+
+function fetchAndDisplay() {
+  fetch("https://fakestoreapi.com/products")
+    .then((response: Response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch Data");
+      }
+      return response.json();
+    })
+    .then((data: IProduct[]) => {
+      data.forEach((product: IProduct) => {
+        console.log(product.image);
+        const cardContainer = document
+          .querySelector("#itemContainer")!
+          .cloneNode(true) as HTMLElement;
+
+        (cardContainer.querySelector("#img") as HTMLImageElement).src =
+          product.image;
+
+        (
+          cardContainer.querySelector("#productName") as HTMLElement
+        ).textContent = product.title;
+
+        (cardContainer.querySelector("#price") as HTMLElement).textContent =
+          product.price.toString();
+
+        productsContainer.appendChild(cardContainer);
+      });
+    });
+}
